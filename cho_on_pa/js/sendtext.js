96fb20c868f6b830338d115db71c80a7f60d7018
Ryujiro Sakai <sakateu@gmail.com>
2017-12-10 03:01:29 +0900

    オートフォーカス

--- cho_on_pa/js/sendtext.js
+++ cho_on_pa/js/sendtext.js
@@ -103,7 +103,7 @@ var TextTransmitter = (function() {
     };
 
     function onDOMLoad() {
-      console.log("commit 2:57");
+      console.log("commit 3:00");
         btn = document.querySelector('[data-quiet-send-button]');
         stp_btn = document.querySelector('[data-quiet-send-stop-button]');
         selectbox = document.querySelector('[data-quiet-select-input]');
